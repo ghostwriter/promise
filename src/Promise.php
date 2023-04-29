3254ef79034171d8d584c9bf8b809dd809213f73
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-29 16:26:33 -0500

    Update Promise.php

--- src/Promise.php
+++ src/Promise.php
@@ -4,94 +4,258 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Promise;
 
+use Closure;
+use Ghostwriter\Promise\State\Fulfilled;
+use Ghostwriter\Promise\State\FulfilledInterface;
+use Ghostwriter\Promise\State\Pending;
+use Ghostwriter\Promise\State\PendingInterface;
+use Ghostwriter\Promise\State\Rejected;
+use Ghostwriter\Promise\State\RejectedInterface;
+use Ghostwriter\Promise\State\StateInterface;
+use RuntimeException;
 use Throwable;
 
-/** @see PromiseTest */
+/**
+ * @template TPromise
+ *
+ * @implements PromiseInterface<TPromise>
+ *
+ * @see PromiseTest
+ */
 final class Promise implements PromiseInterface
 {
-    public const FULFILLED = 'fulfilled';
+    /** @var Closure(Throwable):never */
+    private Closure $defaultReject;
 
-    public const PENDING = 'pending';
+    /** @var Closure(TPromise):TPromise */
+    private Closure $defaultResolve;
 
-    public const REJECTED = 'rejected';
+    /**
+     * @var array<array-key,Closure(TPromise):TPromise>
+     */
+    private array $finallyCallbacks = [];
 
+    /**
+     * @var array<array-key,Closure(TPromise):TPromise>
+     */
     private array $fulfilledCallbacks = [];
 
+    /**
+     * @var array<array-key,Closure(Throwable):Throwable>
+     */
     private array $rejectedCallbacks = [];
 
-    private mixed $result = null;
+    private StateInterface $state;
 
-    private string $state = self::PENDING;
+    /**
+     *
+     * @template TPromise
+     * @template TPromiseResult
+     * @template TSuccessResult
+     *
+     * @var Closure(TPromise):null|TSuccessResult
+     * @var Closure(Throwable):null|never
+     *
+     * @param null|Closure(Closure(TPromise):TPromiseResult,Closure(Throwable):never):void $resolver
+     * @param null|Closure                                                                 $canceller
+     */
+    public function __construct(
+        private Closure|null $resolver = null,
+        private Closure|null $canceller = null
+    ) {
+        $this->transitionTo(new Pending());
 
-    public function catch(callable $onRejected): PromiseInterface
-    {
-        return $this->then(null, $onRejected);
+        /** @var Closure(TPromise):TPromise $this->defaultResolve */
+        $this->defaultResolve = static fn (
+            /**
+             * @param TPromise $reason
+             *
+             * @return TPromise
+             */
+            mixed $value
+        ): mixed => $value;
+
+        $this->defaultReject = static fn (Throwable $reason): never => throw $reason;
+
+        //
+        //        $onFulfilled ??= static fn (mixed $value): mixed => $value;
+        //
+        //        $onRejected ??= static fn (Throwable $throwable): never => throw $throwable;
+
+        //        $executor ??= static fn (Closure $_, Closure $__): mixed => null;
+        //
+        //        try {
+        //            $executor($this->defaultResolve, $this->defaultReject);
+        //        } catch (Throwable $throwable) {
+        //            $this->reject($throwable);
+        //        }
     }
 
-    public function finally(callable $onFulfilledOrRejected): PromiseInterface
+    public static function all(iterable $promises): PromiseInterface
     {
+        return new self();
+    }
+
+    public static function any(iterable $promises): PromiseInterface
+    {
+        return new self();
+    }
+
+    public function catch(Closure $onRejected): self
+    {
+        return $this->then($this->defaultResolve, $onRejected);
+    }
+
+    public function finally(Closure $onFulfilledOrRejected): self
+    {
+        $this->finallyCallbacks[] = $onFulfilledOrRejected;
+
         return $this->then($onFulfilledOrRejected, $onFulfilledOrRejected);
     }
 
-    public function reject($reason)
+    public function getReason(): Throwable
     {
-        if ($this->state === self::PENDING) {
-            $this->state = self::REJECTED;
-            $this->result = $reason;
-            foreach ($this->rejectedCallbacks as [$callback, $promise]) {
-                $this->executeCallback($callback, $promise, $this->result);
-            }
-        }
+        return $this->result;
     }
 
-    public function resolve($value)
+    public function getResult(): mixed
     {
-        if ($this->state === self::PENDING) {
-            $this->state = self::FULFILLED;
-            $this->result = $value;
-            foreach ($this->fulfilledCallbacks as [$callback, $promise]) {
-                $this->executeCallback($callback, $promise, $this->result);
-            }
-        }
+        return $this->result;
     }
 
-    public function then(callable $onFulfilled = null, callable $onRejected = null): PromiseInterface
+    public function getState(): StateInterface
     {
-        switch ($this->state) {
-            case self::FULFILLED:
-                $this->executeCallback($onFulfilled, $this, $this->result);
+        return $this->state;
+    }
+
+    public function isFulfilled(): bool
+    {
+        return $this->state instanceof FulfilledInterface;
+    }
+
+    public function isPending(): bool
+    {
+        return $this->state instanceof PendingInterface;
+    }
+
+    public function isRejected(): bool
+    {
+        return $this->state instanceof RejectedInterface;
+    }
+
+    public function map(Closure $onFulfilled): self
+    {
+        return $this->then($onFulfilled, $this->defaultReject);
+    }
+
+    public static function race(iterable $promises): PromiseInterface
+    {
+        return new self();
+    }
+
+    public function reject(Throwable $throwable): void
+    {
+        $state = &$this->state;
+        if (! $state instanceof PendingInterface) {
+            return;
+        }
+
+        $state = new Rejected($throwable);
+
+        array_map([$this, 'executeCallback'], $this->rejectedCallbacks);
+        //        array_walk($this->rejectedCallbacks, [$this, 'executeCallback']);
+        //        foreach ($this->rejectedCallbacks as $callback) {
+        //            $this->executeCallback($callback);
+        //        }
+    }
+
+    public function resolve(mixed $value): void
+    {
+        $state = &$this->state;
+        if (! $state instanceof PendingInterface) {
+            return;
+        }
+        $state = new Fulfilled($value);
+
+        array_map([$this, 'executeCallback'], $this->fulfilledCallbacks);
+        //        foreach ($this->fulfilledCallbacks as $callback) {
+        //            $this->executeCallback($callback);
+        //        }
+    }
+
+    /**
+     * @template TResult
+     *
+     * @param Closure(TPromise):null|TResult  $onFulfilled
+     * @param Closure(Throwable):null|TResult $onRejected
+     *
+     * @param-out self<TPromise|TResult|Throwable>
+     *
+     * @return self<Throwable|TPromise|TResult>
+     */
+    public function then(Closure $onFulfilled, Closure $onRejected): self
+    {
+        switch (true) {
+            case $this->state instanceof FulfilledInterface:
+                $this->executeCallback($onFulfilled);
                 break;
-            case self::REJECTED:
-                $this->executeCallback($onRejected, $this, $this->result);
+            case $this->state instanceof RejectedInterface:
+                $this->executeCallback($onRejected);
                 break;
-            case self::PENDING:
-                $this->fulfilledCallbacks[] = [$onFulfilled, $this];
-                $this->rejectedCallbacks[] = [$onRejected, $this];
+            case $this->state instanceof PendingInterface:
+                $this->fulfilledCallbacks[] = $onFulfilled;
+                $this->rejectedCallbacks[] = $onRejected;
                 break;
+            default:
+                throw new RuntimeException('Shold not happen!');
         }
 
         return $this;
     }
 
-    private function executeCallback(?callable $callback, self $promise, $value)
+    public function wait(): mixed
     {
-        if (! is_callable($callback)) {
-            if ($this->state === self::FULFILLED) {
-                $promise->resolve($value);
-            } else {
-                $promise->reject($value);
-            }
-            return;
+        $state = $this->state;
+
+        return match (true) {
+            $state instanceof FulfilledInterface => $state->getValue(),
+            $state instanceof RejectedInterface => $state->getReason(),
+            default => null
+        };
+    }
+
+    /**
+     * @template TResult of Throwable|TPromise
+     *
+     * @param null|Closure(Throwable|TPromise):TResult $callback
+     */
+    private function executeCallback(Closure $callback): void
+    {
+        /** @var FulfilledInterface|RejectedInterface $state */
+        $state = $this->state;
+        if ($state instanceof PendingInterface) {
+            throw new RuntimeException('Should not happen.');
         }
+
         try {
-            $result = $callback($value);
+            $result = $callback(match (true) {
+                $state instanceof FulfilledInterface => $state->getValue(),
+                $state instanceof RejectedInterface => $state->getReason(),
+            });
+
             if ($result instanceof PromiseInterface) {
-                $result->then([$promise, 'resolve'], [$promise, 'reject']);
-            } else {
-                $promise->resolve($result);
+                $result->then([$this, 'resolve'], [$this, 'reject']);
+                return;
             }
-        } catch (Throwable $e) {
-            $promise->reject($e);
+
+            $this->resolve($result);
+        } catch (Throwable $throwable) {
+            $this->reject($throwable);
         }
+    }
+
+    private function transitionTo(StateInterface $state): void
+    {
+        $this->state = $state;
     }
 }
