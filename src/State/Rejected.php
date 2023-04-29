71223a236a5c0dcc050a0e5b3788376c1cf54b7c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-29 16:17:41 -0500

    Create Rejected.php

---
+++ src/State/Rejected.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Promise\State;
+
+use Throwable;
+
+final class Rejected implements RejectedInterface
+{
+    public function __construct(
+        private readonly Throwable $throwable
+    ) {
+    }
+
+    public function getReason(): Throwable
+    {
+        return $this->throwable;
+    }
+}
