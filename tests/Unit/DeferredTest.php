5cb0999118243e2687d01084f9124401173236b7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-29 14:07:20 -0500

    Update DeferredTest.php

--- tests/Unit/DeferredTest.php
+++ tests/Unit/DeferredTest.php
@@ -5,25 +5,77 @@ declare(strict_types=1);
 namespace Ghostwriter\Promise\Tests\Unit;
 
 use Ghostwriter\Promise\Deferred;
+use Ghostwriter\Promise\DeferredInterface;
 use Ghostwriter\Promise\Promise;
 use Ghostwriter\Promise\PromiseInterface;
+use Ghostwriter\Promise\State\Fulfilled;
+use Ghostwriter\Promise\State\Rejected;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use Throwable;
 
 #[CoversClass(Deferred::class)]
 #[UsesClass(Promise::class)]
+#[UsesClass(Rejected::class)]
+#[UsesClass(Fulfilled::class)]
 final class DeferredTest extends TestCase
 {
-    public function testReturnsDeferredInstance()
+    private DeferredInterface $deferred;
+
+    protected function setUp(): void
     {
-        self::assertInstanceOf(Deferred::class, new Deferred());
+        $this->deferred = new Deferred();
     }
 
-    public function testReturnsPromise()
+    protected function tearDown(): void
+    {
+        $this->deferred = new Deferred();
+    }
+
+    public function testInstance(): void
+    {
+        self::assertInstanceOf(Deferred::class, $this->deferred);
+        self::assertInstanceOf(DeferredInterface::class, $this->deferred);
+    }
+
+    public function testRejectPromise(): void
+    {
+        $this->deferred->getPromise()
+            ->then(
+                static function (mixed $value): void {
+                    self::fail('Should not happen! got: ' . get_debug_type($value));
+                },
+                static function (Throwable $throwable): void {
+                    self::assertSame('fail.', $throwable->getMessage());
+                }
+            );
+        $this->deferred->reject(new RuntimeException('fail.'));
+    }
+
+    public function testResolvePromise(): void
     {
         $deferred = new Deferred();
-        $promise = $deferred->promise();
+
+        $deferred->getPromise()
+            ->then(
+                static function (mixed $value): void {
+                    self::assertTrue($value);
+                    self::assertFalse($value);
+                    self::assertSame('wth?', $value);
+                },
+                static function (Throwable $throwable): void {
+                    self::fail($throwable->getMessage());
+                }
+            );
+
+        $deferred->resolve('pass');
+    }
+
+    public function testReturnDeferredPromise(): void
+    {
+        $promise = $this->deferred->getPromise();
         self::assertInstanceOf(Promise::class, $promise);
         self::assertInstanceOf(PromiseInterface::class, $promise);
     }
